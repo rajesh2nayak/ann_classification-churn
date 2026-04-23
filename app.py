ddaa1e10a2536cdeff54b6fdd3467caefd63fc5d
rajesh2nayak <rajesh2nayak@gmail.com>
2026-04-24 01:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import streamlit as st
 import tensorflow as tf
 
 #model = tf.keras.models.load_model('churn_model.h5')
-model = tf.keras.models.load_model('model.keras')
+model = tf.keras.models.load_model("model.keras")
+print("Model loaded successfully")
 
 #label the encode the scaled data
 with open('label_encoder_gender.pkl', 'rb') as file:
