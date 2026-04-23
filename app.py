fb9a4ae71e17bb872e9cc9596cedea3ad1a41974
rajesh2nayak <rajesh2nayak@gmail.com>
2026-04-24 00:33:11 +0530

    Fix model loading function call

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 from sklearn.preprocessing import LabelEncoder
 import streamlit as st
 
-model = tf.keras.models.load_model('churn_model.h5')
+model = load_model('churn_model.h5') 
 
 #label the encode the scaled data
 with open('label_encoder_gender.pkl', 'rb') as file:
